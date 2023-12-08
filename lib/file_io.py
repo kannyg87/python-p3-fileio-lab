51f14109307002bbcb789fd02cc8e176ae4e80d7
Kanny <kanny.ghafour87@gmail.com>
2023-12-08 12:33:16 -0600

    lab copmleted

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,17 @@
 def write_file(file_name, file_content):
-    pass
+    # with open(f'{file_name}.txt', 'w') as f:
+    #     f.write(file_content)
+    f = open(f'{file_name}.txt', 'w')
+    f.write(file_content)
+    f.close()
 
 def append_file(file_name, append_content):
-    pass
+    f = open(f'{file_name}.txt', 'a')
+    f.write(append_content)
+    f.close()
 
 def read_file(file_name):
-    pass
+    f= open(f'{file_name}.txt', 'r')
+    content = f.read()
+    f.close()
+    return content
